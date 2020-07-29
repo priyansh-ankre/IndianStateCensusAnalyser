2bf1f4c713a8e293040a95f9106fdee8f8368c7d
priyansh-ankre <bod123priyansh@gmail.com>
2020-07-29 16:12:52 +0530

    [Priyansh]Added:gitignore file

---
+++ IndiaStateCensusAnalyser/Program.cs
@@ -0,0 +1,12 @@
+﻿using System;
+
+namespace IndiaStateCensusAnalyser
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello World!");
+        }
+    }
+}
