428f463b6b709ddc2c6badb334a7e106398f59c9
priyansh-ankre <bod123priyansh@gmail.com>
2020-07-30 23:33:25 +0530

    sdfds

---
+++ IndiaStateCensusAnalyser/JSONCode.cs
@@ -0,0 +1,14 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace IndiaStateCensusAnalyser
+{
+    class JSONCode
+    {
+        public string SrNo { get; set; }
+        public string StateName { get; set; }
+        public string TIN { get; set; }
+        public string StateCode { get; set; }
+    }
+}
--- IndiaStateCensusAnalyser/Program.cs
+++ IndiaStateCensusAnalyser/Program.cs
@@ -6,16 +6,17 @@ namespace IndiaStateCensusAnalyser
     {
         public static void Main(string[] args)
         {
-            string FILE_PATH = @"C:\Users\hp\source\repos\IndiaStateCensusAnalyserApplication\IndiaStateCensusAnalyser\CSVfiles\IndiaStateCensusData.csv";
-
-            int csvStateCensusRecords = CSVStateCensus.GetRecords(FILE_PATH);
-            int stateCensusRecords = StateCensusAnalyser.GetStateCensusRecords(FILE_PATH);
+            string STATE_CENSUS_FILE_PATH = @"C:\Users\hp\source\repos\IndiaStateCensusAnalyserApplication\IndiaStateCensusAnalyser\CSVfiles\IndiaStateCensusData.csv";
+            string STATE_CODE_FILE_PATH = @"C:\Users\hp\source\repos\IndiaStateCensusAnalyserApplication\IndiaStateCensusAnalyser\CSVfiles\IndiaStateCode.csv";
+            int csvStateCensusRecords = CSVStateCensus.GetRecords(STATE_CENSUS_FILE_PATH);
+            int stateCensusRecords = StateCensusAnalyser.GetStateCensusRecords(STATE_CENSUS_FILE_PATH);
             Console.WriteLine("CSV state census records: " + csvStateCensusRecords);
             Console.WriteLine("state census recors: " + stateCensusRecords);
-            StateCensusAnalyser.GetData(FILE_PATH);
+            StateCensusAnalyser.GetData(STATE_CENSUS_FILE_PATH);
             Console.WriteLine();
-            new JSONStateCensus(FILE_PATH).SortByState();
-            
+            new JSONStateCensus(STATE_CENSUS_FILE_PATH).SortIndiaSateCensusByState();
+            Console.WriteLine();
+            new JSONStateCensus(STATE_CENSUS_FILE_PATH).SortIndiaSateCensusByDensityPerSqKm();
         }
     }
 }
