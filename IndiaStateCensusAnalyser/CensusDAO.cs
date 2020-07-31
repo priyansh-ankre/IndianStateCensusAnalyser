46aceb53a9b5de4f006a85f38001afecc90326c7
priyansh-ankre <64030924+priyansh-ankre@users.noreply.github.com>
2020-07-31 22:32:17 +0530

    [Priyansh]Added:US census csv file to read

--- IndiaStateCensusAnalyser/CensusDAO.cs
+++ IndiaStateCensusAnalyser/CensusDAO.cs
@@ -1,4 +1,4 @@
-﻿using System;
+using System;
 using System.Collections.Generic;
 using System.Text;
 
@@ -10,9 +10,13 @@ namespace IndiaStateCensusAnalyser
         public string Population { get; set; }
         public string AreaInSqKm { get; set; }
         public string DensityPerSqKm { get; set; }
-        public string SrNo { get; set; }
         public string StateName { get; set; }
-        public string TIN { get; set; }
-        public string StateCode { get; set; }
+        public string StateId { get; set; }
+        public string HousingUnits { get; set; }
+        public string TotalArea { get; set; }
+        public string WaterArea { get; set; }
+        public string LandArea { get; set; }
+        public string PopulationDensity { get; set; }
+        public string HousingDensity { get; set; }
     }
 }
--- IndiaStateCensusAnalyser/ICSVHelper.cs
+++ IndiaStateCensusAnalyser/ICSVHelper.cs
@@ -12,4 +12,3 @@ namespace IndiaStateCensusAnalyser
         public string SortIndiaStateCensusByAreaInSqKm();
     }
 }
-
